fbf0c4f5c32357a0b01a78a2de4ec4ff104d17a3
Mohammad Umair Khan <mohammad.umair.k@gmail.com>
2016-10-25 11:31:29 +0500

    fix(after-hook): convert types to any
    after hook pipeline will take an any argument and return an any argument, because the pipeline can do any modifications to the object and it will still be valid in the after hook pipeline

--- src/ng2-http-client.types.ts
+++ src/ng2-http-client.types.ts
@@ -1,4 +1,4 @@
 import { Request, Response } from '@angular/http';
 
 export declare type BeforeHookFunction = (req: Request) => void
-export declare type AfterHookFunction = (res: Response) => Response
+export declare type AfterHookFunction = (res: any) => any
